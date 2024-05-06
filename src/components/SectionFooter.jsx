82203859acd8a082b2d28ece233ec24b64318b3f
anoop <anoopano20@gmail.com>
2024-05-06 10:38:27 +0530

    more styling fixes

--- src/components/SectionFooter.jsx
+++ src/components/SectionFooter.jsx
@@ -2,12 +2,14 @@ import Button from "./ui/Button"
 import "../styles/SectionFooter.css"
 
 export const SectionFooter = () => {
-    return(
-        <footer className="section-footer">
-            <div>
-                <span className="description">POWERED BY APPOINTO</span>
-            </div>
-            <Button text="Next"/>
-        </footer>
-    )
+    return (
+      <footer className="section-footer">
+        <div>
+          <span className="description">
+            POWERED BY <a href="https://www.appointo.me/">APPOINTO</a>
+          </span>
+        </div>
+        <Button text="Next" />
+      </footer>
+    );
 }
