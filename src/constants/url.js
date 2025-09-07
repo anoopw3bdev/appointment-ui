5cfcbc79f7b96a9819a6b8639fca5a55691a5df5
anoop <anoopano20@gmail.com>
2025-09-07 22:55:57 +0530

    Updates

--- src/constants/url.js
+++
@@ -1,4 +0,0 @@
-export const getTimeSlotsUrl = (startDate, endDate) => {
-  const url = `https://app.appointo.me/scripttag/mock_timeslots?start_date=${startDate}&end_date=${endDate}`;
-  return url;
-};
